9f14c7cb26b11fd5cea793ef3482e7a9f08e761c
4lch4 <git@4lch4.dev>
2021-03-27 15:35:08 -0500

    feat: basic version of the list command working

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,25 +1,31 @@
-import {Command, flags} from '@oclif/command'
+// import Table from 'cli-table3'
+// const table = new Table()
+import { BaseCommand } from '../lib'
+import { DefaultSSHDir } from '../lib/Constants'
+import { IOUtil } from '../lib/utils/IOUtil'
 
-export default class List extends Command {
-  static description = 'describe the command here'
+export default class List extends BaseCommand {
+  static description = 'List all of your SSH keys in your ~/.ssh dir.'
 
   static flags = {
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    ...BaseCommand.flags
   }
 
-  static args = [{name: 'file'}]
-
   async run() {
-    const {args, flags} = this.parse(List)
+    const { args, flags } = this.parse(List)
+    const ioUtil = IOUtil.newInstance(DefaultSSHDir)
+    const keys = await ioUtil.getKeys()
+    // this.debug(`keys.length = ${keys.length}`)
+    // this.debug(JSON.stringify(keys[0], undefined, 2))
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /Users/jisodl0/Development/Projects/alcha/clis/argus/src/commands/list.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    this.exit(0)
+
+    // const name = flags.name ?? 'world'
+    // this.log(
+    //   `hello ${name} from /Users/jisodl0/Development/Projects/alcha/clis/argus/src/commands/list.ts`
+    // )
+    // if (args.file && flags.force) {
+    //   this.log(`you input --force and --file: ${args.file}`)
+    // }
   }
 }
